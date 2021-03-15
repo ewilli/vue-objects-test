9f1d4e152b7ca76481d030edf023692a59800f5e
eck <wilhelm.ecker@ikt.linz.at>
2021-03-15 06:49:34 +0100

    Test

---
+++ src/components/ChildEmit.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="child-emit">
+    <h4>Child with emit</h4>
+    <p>Initial value:</p>
+    {{ initialValue }}
+    <p>Value of child:</p>
+    {{ valueEmit }}
+    <br />
+    <br />
+    Value cat:
+    <input
+      type="text"
+      @input="$emit('update', valueEmit)"
+      v-model="valueEmit.cat"
+    />
+    <br />
+    Value dog:<input
+      type="text"
+      @input="$emit('update', valueEmit)"
+      v-model="valueEmit.dog"
+    />
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+
+@Component({ name: "ChildEmit" })
+export default class HelloWorld extends Vue {
+  @Prop() initialValue!: object;
+
+  valueEmit = JSON.parse(JSON.stringify(this.initialValue));
+}
+</script>
+
+<style scoped>
+.child-emit {
+  color: #b99d42;
+}
+</style>
---
+++ src/components/ChildReference.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="child-reference">
+    <h4>Child with reference as prop</h4>
+    <p>Value of child:</p>
+    {{ valueReference }}
+    <br />
+    <br />
+    Value cat: <input type="text" v-model="valueReference.cat" />
+    <br />
+    Value dog:<input type="text" v-model="valueReference.dog" />
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({ name: "ChildReference" })
+export default class HelloWorld extends Vue {
+  @Prop() private valueReference!: string;
+}
+</script>
+
+<style scoped>
+.child-reference {
+  color: #42b983;
+}
+</style>
